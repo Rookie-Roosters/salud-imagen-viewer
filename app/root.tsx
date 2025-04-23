7d0fa3ef746a1a81d3639894b3679e5066f8329b
manuelgm <manuel.gonzalez.0518@gmail.com>
2025-04-22 23:19:13 -0600

    Add Radix UI themes and components, update styles, and remove welcome page assets

--- app/root.tsx
+++ app/root.tsx
@@ -8,20 +8,9 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import "./app.css";
-
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -33,9 +22,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <Theme accentColor="blue" appearance="dark">
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </Theme>
       </body>
     </html>
   );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Flex, Text, Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Salud Imagen" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Flex direction="column" gap="2">
+    <Text>Hello from Radix Themes :)</Text>
+    <span className="text-4xl font-bold">Salud Imagen</span>
+    <Button><BookmarkIcon />Let's go</Button>
+  </Flex>
 }
+
+
